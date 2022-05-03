6579f162658219c91366b14b97f90f2eb910b046
Hoang Hai <hoangghaii1803@gmail.com>
2022-05-03 16:11:23 +0700

    Fixedcss

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const authRouter = require("./app/routes/auth");
+const siteRouter = require("./app/routes/site");
 
 mongoose.connect(process.env.DB_URL);
 
 app.use(express.json());
+
+app.use("/", siteRouter);
 
 app.use("/auth", authRouter);
 
